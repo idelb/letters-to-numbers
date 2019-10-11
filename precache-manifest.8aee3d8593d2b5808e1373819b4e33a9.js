66833f15c3bc85d09296668fedf031377b62f0e2
Bledar Muca <idelb82@gmail.com>
2019-10-11 20:28:50 +0200

    Updates

---
+++ precache-manifest.8aee3d8593d2b5808e1373819b4e33a9.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b38d679dc962a434dbb639559923d34",
+    "url": "/letters-to-numbers/index.html"
+  },
+  {
+    "revision": "59eb8f695a4957830bfa",
+    "url": "/letters-to-numbers/static/js/2.df380952.chunk.js"
+  },
+  {
+    "revision": "4d0fc0ba72261ea50bd8",
+    "url": "/letters-to-numbers/static/js/main.3bead477.chunk.js"
+  },
+  {
+    "revision": "c59e69e8a1587200a35e",
+    "url": "/letters-to-numbers/static/js/runtime-main.fcde0237.js"
+  }
+]);
